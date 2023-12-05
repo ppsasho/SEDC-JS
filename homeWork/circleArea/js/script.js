3aa0998ed0fb784765e9ad6b8a2952da37267b0b
ppsasho <businessmail5545@gmail.com>
2023-12-05 17:29:07 +0100

    - homework

---
+++ homeWork/circleArea/js/script.js
@@ -0,0 +1,5 @@
+const pi = 3.14;
+let areaValue = prompt('Enter the value for your circle: ');
+areaValue = areaValue ** 2;
+let area = pi * areaValue;
+console.log('The area of the circle is :', area);
