d9d573087b2cb59b78933d9867b135e633bc8ed3
ppsasho <businessmail5545@gmail.com>
2023-12-05 17:48:08 +0100

    - homework

---
+++ homeWork/minutesToSeconds/js/script.js
@@ -0,0 +1,4 @@
+let minutes = prompt('Enter minutes: ');
+const seconds = 60;
+let result = minutes * seconds;
+console.log(minutes,'minutes equal to: ',result, 'seconds.');
---
+++ homeWork/triangleArea/js/script.js
@@ -0,0 +1,4 @@
+let base = prompt('Enter the base value for your triangle: ');
+let height = prompt('Enter the height value for your triangle: ');
+let area = (base * height) / 2;
+console.log('The area of your triangle is: ',area);
