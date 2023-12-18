1a8011495916e4a2cbc552cee38c9f794771813a
ppsasho <businessmail5545@gmail.com>
2023-12-18 17:39:13 +0100

    - homework update

--- homeWork/class 06/js/script.js
+++ homeWork/class 06/js/script.js
@@ -1,16 +1,25 @@
 let paragraph = document.getElementById("paragraph");
 let table = document.getElementById("table");
-table.style.border = "8px outset";
 
 let inputRows = Number(prompt("enter amount of rows:"));
 let inputCollumns = Number(prompt("enter amount of collumns:"));
-let tableCells;
-paragraph.innerText = `This table has ${inputRows} rows and ${inputCollumns} collumns.`;
 
-for (let i = 0; i < inputRows; i++) {
-    let tableRow = table.insertRow(i);
-    for (let b = 0; b < inputCollumns; b++) {
-        tableCells = tableRow.insertCell(b);
-        tableCells.innerText += `Row-${i+1} Collumn-${b + 1}`;
+if (!isNaN(inputRows && inputCollumns) === true) {
+    table.style.border = "8px outset";
+    let tableCells;
+    paragraph.innerText = `This table has ${inputRows} rows and ${inputCollumns} collumns.`;
+    
+    for (let i = 0; i < inputRows; i++) {
+        let tableRow = table.insertRow(i);
+        for (let b = 0; b < inputCollumns; b++) {
+            tableCells = tableRow.insertCell(b);
+            tableCells.innerText += `Row-${i+1} Collumn-${b + 1}`;
+        }
     }
+    
+}
+else {
+    alert(
+        "You need to enter digits only!"
+    );
 }
